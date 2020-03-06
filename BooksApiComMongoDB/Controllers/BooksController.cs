340d51f9a80f3eab7562857f61b4d4474a75d6db
gionbritto <gio.nbritto@gmail.com>
2020-03-06 08:50:24 -0300

    Projeto .Net Core WebApi CRUD utilizando MongoDB

---
+++ BooksApiComMongoDB/Controllers/BooksController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BooksApiComMongoDB.Models;
+using BooksApiComMongoDB.Servicos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksApiComMongoDB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : Controller
+    {
+        private readonly BookService _bookService;
+        public BooksController(BookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        public ActionResult<List<Book>> Get() =>
+            _bookService.Get();
+
+        [HttpGet("{id:length(24)}", Name = "GetBook")]
+        public ActionResult<Book> Get(string id)
+        {
+            var book = _bookService.Get(id);
+            if(book == null)
+            {
+                return NotFound();
+            }
+            return book;
+        }
+
+        [HttpGet("{id:length(24)}")]
+        public IActionResult Update(string id, Book bookIn)
+        {
+            var book = _bookService.Get(id);
+
+            if(book == null)
+            {
+                return NotFound();
+            }
+
+            _bookService.Update(id, bookIn);
+
+            return NoContent();
+        }
+
+        [HttpGet("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var book = _bookService.Get(id);
+
+            if(book == null)
+            {
+                return NotFound();
+            }
+
+            _bookService.Remove(book.Id);
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public void Add()
+        {
+            InserirDados();
+        }
+        private void InserirDados()
+        {
+            Book book = new Book()
+            {
+                BookName = "Design Patterns",
+                Price = 54.93M,
+                Category = "Computers",
+                Author = "Ralph Johnson"
+            };
+            _bookService.Create(book);
+            //insertMany([{ 'Name':'Design Patterns', 'Price':54.93,'Category':'Computers', 'Author':'Ralph Johnson'}, { 'Name':'Clean Code','Price':43.15,'Category':'Computers','Author':'Robert C. Martin'}])
+        }
+    }
+}
---
+++ BooksApiComMongoDB/Servicos/BookService.cs
@@ -0,0 +1,45 @@
+﻿using BooksApiComMongoDB.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksApiComMongoDB.Servicos
+{
+    //Classe de serviço responsável pelas operações de crud
+    public class BookService
+    {
+        //diz que vou tratar de uma coleção especifica (book)
+        private readonly IMongoCollection<Book> _books;
+
+        public BookService(IBookstoreDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _books = database.GetCollection<Book>(settings.BooksCollectionName);
+        }
+
+        public List<Book> Get() => 
+            _books.Find(book => true).ToList();
+
+        public Book Get(string id) =>
+            _books.Find(b => b.Id == id).FirstOrDefault();
+
+        public Book Create(Book book)
+        {
+            _books.InsertOne(book);
+            return book;
+        }
+
+        public void Update(string id, Book bookIn) =>
+            _books.ReplaceOne(b => b.Id == id, bookIn);
+
+        public void Remove(Book bookIn) =>
+            _books.DeleteOne(b => b.Id == bookIn.Id);
+
+        public void Remove(string id) =>
+            _books.DeleteOne(b => b.Id == id);
+    }
+}
